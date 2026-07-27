d7a866e491a8ffe66f860a515b4e1f9add1916d5
SHAKTHI VEL S <shakthivelmpl550@gmail.com>
2026-07-27 14:36:01 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,109 +1,98 @@
 #include <LiquidCrystal.h>
 
-// Pins
 #define BUTTON 4
 #define LED 2
 #define BUZZER 15
 
-// LCD pins
-LiquidCrystal lcd(21, 22, 19, 23, 25, 26);
+LiquidCrystal lcd(21,22,19,23,25,26);
 
-
-// Variables
 int students = 0;
+int maxSeats = 50;
+int availableSeats = 0;
 
+int fullCount = 0;
 
 void setup()
 {
   Serial.begin(115200);
 
   pinMode(BUTTON, INPUT_PULLUP);
-
   pinMode(LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-
 
   lcd.begin(16,2);
 
   lcd.setCursor(0,0);
   lcd.print("BUS MONITOR");
-
   lcd.setCursor(0,1);
   lcd.print("Starting...");
 
   delay(2000);
-
   lcd.clear();
 
   displayData();
 }
 
-
-
 void loop()
 {
-
-  // Student entry button
   if(digitalRead(BUTTON) == LOW)
   {
-
     students++;
+
+    availableSeats = maxSeats - students;
 
     Serial.print("Students : ");
     Serial.println(students);
 
+    Serial.print("Available Seats : ");
+    Serial.println(availableSeats);
 
     displayData();
 
-
-    // wait for button release
     while(digitalRead(BUTTON)==LOW)
     {
       delay(10);
     }
 
-
     delay(300);
   }
-
 }
 
-
-
-// Display function
 void displayData()
 {
-
   lcd.clear();
 
-
   lcd.setCursor(0,0);
-  lcd.print("Students:");
+  lcd.print("Stud:");
   lcd.print(students);
 
-
-
-  lcd.setCursor(0,1);
-
+  lcd.setCursor(10,0);
+  lcd.print("A:");
+  lcd.print(availableSeats);
 
   if(students > 50)
   {
+    fullCount++;
+  }
+  else
+  {
+    fullCount = 0;
+  }
 
+  lcd.setCursor(0,1);
+
+  if(fullCount >= 3)
+  {
     lcd.print("FULL ALERT");
 
     digitalWrite(LED,HIGH);
     digitalWrite(BUZZER,HIGH);
-
   }
-
   else
   {
-
     lcd.print("NORMAL");
 
     digitalWrite(LED,LOW);
     digitalWrite(BUZZER,LOW);
-
   }
-
 }
